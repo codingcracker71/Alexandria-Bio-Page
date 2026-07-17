2948e2761504da4ec3b04677e05e8ef962fa934d
codingcracker71 <grahamalexandria91@gmail.com>
2026-07-16 20:34:31 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
+function myFunction() {
+    var x = document.getElementById("fact");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+// I added a "Fun Fact" button to the "My interests" secction to highlight something related to my interests. 
+// When the button is clicked, it shows a fun fact section related to my last interest. Javascript selects the div element and the event listener responds to the click for the following id "fact".
+
 
